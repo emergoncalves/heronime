10bb02e3849a77b93d331064686e675e87fcef6e
Emerson Gonçalves <emerson.u.g@gmail.com>
2022-11-08 14:32:49 -0300

    add infinite scroll

--- components/ListContent/index.js
+++ components/ListContent/index.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ContentCard from '../ContentCard'
 import styles from '../../styles/ListContent.module.less'
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default function ListContent({ results }) {
+  const [posts, setPosts] = useState(results.data);
+  const [hasMore, setHasMore] = useState(true);
+
+  const getMorePost = async () => {
+    const res = await fetch(
+      `https://kitsu.io/api/edge/anime?page[limit]=10&page[offset]=${posts.length}`
+    );
+    const newPosts = await res.json();
+    console.log(newPosts)
+    setPosts((post) => [...post, ...newPosts.data]);
+  };
+
   return (
-    <div className={styles.listContent}>
-      {results.data.map(result =>
-        <ContentCard key={result.id} info={result.attributes} />
-      )}
-    </div>
+    <InfiniteScroll
+      dataLength={posts.length}
+      next={getMorePost}
+      hasMore={hasMore}
+      loader={<h3> Loading...</h3>}
+      endMessage={<h4>Nothing more to show</h4>}
+    >
+      <div className={styles.listContent}>
+        {posts.map(result =>
+          <ContentCard key={result.id} info={result.attributes} />
+        )}
+      </div>
+    </InfiniteScroll>
   )
 }
