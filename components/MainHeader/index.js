5ec212958540cf06b7d302c8dc796e31258fb236
Emerson Gonçalves <emerson.u.g@gmail.com>
2022-11-08 22:30:22 -0300

    Refactor

--- components/MainHeader/index.js
+++ components/MainHeader/index.js
@@ -1,15 +1,12 @@
-import React, {useState} from 'react';
-import { useRouter } from 'next/router'
+import React from 'react';
 import { Layout, Space } from 'antd'
-import Head from 'next/head'
 import LogoImage from '../LogoImage';
-import styles from '../../styles/Home.module.less';
 import SearchInput from '../SearchInput';
 import Link from 'next/link';
 
 function MainHeader() {
   const { Header } = Layout;
-  const router = useRouter();
+
   return (
     <>
       <Header className='container header'>
